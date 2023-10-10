c84a660e5f4abc624c3dd2453a62d3ee5acb117a
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2023-10-10 08:58:37 +0530

    Banner Page (Work In Progress)

--- public/Admin/include/header.php
+++ public/Admin/include/header.php
@@ -10,7 +10,7 @@ include "../config/conn.php";
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title><?= $pageTitle ?></title>
+  <title>Sneaker Head | <?= $pageTitle ?></title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -28,7 +28,7 @@ include "../config/conn.php";
   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
   <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
   <style>
-    .nav-item .nav.nav-treeview .nav-item {
+    .nav-item .nav.nav-treeview .nav-item .nav-link {
         padding-left: 25px;
     }
   </style>
---
+++ public/Admin/pages/banner.php
@@ -0,0 +1,84 @@
+<?php $pageTitle = 'Banner'; ?>
+<?php include '../include/header.php' ?>
+
+<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+
+
+        <?php include '../include/preloader.php'; ?>
+
+        <?php include '../include/navbar.php'; ?>
+
+        <?php include '../include/sidebar.php'; ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">Banner</h1>
+                        </div><!-- /.col -->
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                <li class="breadcrumb-item active">Banner</li>
+                            </ol>
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="card col-md-12">
+                            <div class="card-header">
+                                <h3 class="card-title">Create Banner</h3>
+                            </div>
+                            <div class="card-body">
+                                <form action="" class="row" method="POST">
+                                    <div class="col-md-6 form-group">
+                                        <label class="form-label" for="">Image</label>
+                                        <input class="form-control" type="file" name="image" id="">
+                                    </div>
+                                    <div class="col-md-6 form-group">
+                                        <label class="form-label" for="">Image 2</label>
+                                        <input class="form-control" type="file" name="image_2" id="">
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="card col-md-12">
+                            <div class="card-header">
+                                <h3 class="card-title">Banner List</h3>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered table-hover">
+                                        <thead>
+                                            <th>Sr. No</th>
+                                            <th>Image</th>
+                                            <th>Date</th>
+                                            <th>Action</th>
+                                        </thead>
+                                        <tbody></tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--/. container-fluid -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <?php include '../include/footer.php'; ?>
+
+</body>
+
+</html>
