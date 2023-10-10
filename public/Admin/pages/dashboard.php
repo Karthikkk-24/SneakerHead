4be0d02f8fe59cb81a519579ed0e3e5bfd4cce4e
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2023-10-10 08:39:35 +0530

    Added Preloader as include file, identation in sidebar

--- public/Admin/pages/dashboard.php
+++ public/Admin/pages/dashboard.php
@@ -3,10 +3,8 @@
 <body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
 <div class="wrapper">
 
-  <!-- Preloader -->
-  <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__wobble" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
-  </div>
+  
+  <?php include '../include/preloader.php'; ?>
 
   <?php include '../include/navbar.php'; ?>
 
