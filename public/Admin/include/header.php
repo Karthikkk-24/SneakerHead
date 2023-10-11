41b65893c3d49d82410dc38360718df13a88e34c
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2023-10-11 08:09:09 +0530

    Added Styles Page for Admin, Created page for categories and sub_categories

--- public/Admin/include/header.php
+++ public/Admin/include/header.php
@@ -22,14 +22,9 @@ include "../config/conn.php";
   <!-- Theme style -->
   <link rel="stylesheet" href="../dist/css/adminlte.min.css">
   <!-- custom css -->
-  <link rel="stylesheet" href="../custom/css/style.css">
+  <link rel="stylesheet" href="../assets/css/style.css">
   <!-- Select 2 -->
   <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
   <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-  <style>
-    .nav-item .nav.nav-treeview .nav-item .nav-link {
-        padding-left: 25px;
-    }
-  </style>
 </head>
---
+++ public/Admin/pages/categories.php
@@ -0,0 +1,79 @@
+<?php $pageTitle = 'Banner'; ?>
+<?php include '../include/header.php' ?>
+
+<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+
+
+        <?php include '../include/preloader.php'; ?>
+
+        <?php include '../include/navbar.php'; ?>
+
+        <?php include '../include/sidebar.php'; ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">Category</h1>
+                        </div><!-- /.col -->
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                <li class="breadcrumb-item active">Category</li>
+                            </ol>
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="card col-md-12">
+                            <div class="card-header">
+                                <h3 class="card-title">Create Category</h3>
+                            </div>
+                            <div class="card-body">
+                                <form action="" class="row" method="POST">
+                                    <div class="col-md-6 form-group">
+                                        <label class="form-label" for="">Category Name</label>
+                                        <input class="form-control" type="text" placeholder="Enter Category Name" name="category_name" id="">
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="card col-md-12">
+                            <div class="card-header">
+                                <h3 class="card-title">Category List</h3>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered table-hover">
+                                        <thead>
+                                            <th>Sr. No</th>
+                                            <th>Category Name</th>
+                                            <th>Action</th>
+                                        </thead>
+                                        <tbody></tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--/. container-fluid -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <?php include '../include/footer.php'; ?>
+
+</body>
+
+</html>
