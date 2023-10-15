f76a93773326842aa3b97d5a75f4960494101203
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2023-10-15 23:19:13 +0530

    Added Cards and Card Animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,16 @@
 const bannerContainer = document.getElementById('bannerContainer');
 
+const cardsContainer = document.querySelectorAll('.card');
+
+cardsContainer.forEach((element) => {
+    // element.addEventListener('mouseover', () => {
+    //     element.firstElementChild.style.display = "flex";
+    // });
+    // element.addEventListener('mouseout', () => {
+    //     element.firstElementChild.style.display = "none";
+    // })
+});
+
 
 window.addEventListener('load', () => {
     fetchBanner();
@@ -13,8 +24,8 @@ function fetchBanner() {
     fetch ('../../ajax/fetchBanner.php')
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            console.log(data[0][2]);
+            // console.log(data);
+            // console.log(data[0][2]);
             const bannerImage = data[0][2];
             // const parsedResponse = JSON.parse(data);
             bannerContainer.style.backgroundImage = `url(public/${bannerImage})`;
