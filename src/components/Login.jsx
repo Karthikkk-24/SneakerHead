773e232e811dab11a65155fb7ffb7a5db774d0d2
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2023-11-18 07:56:20 +0530

    Login Page Design Changed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,104 @@
 import React from 'react';
-import "../assets/css/login.css";
 
 export default function Login() {
+    
+  const pinkColor = '#f9b4ed';
+  const whiteColor = '#f6fff8';
+  const mainColor = '#d90368';
+  const overlayColor = 'rgba(0, 0, 0, 0.5)';
+
+  
+  const styles = {
+    root: {
+      height: '100vh',
+      width: '100vw',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      background: pinkColor,
+    },
+    loginContainer: {
+      height: '70vh',
+      width: '60vw',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      border: '0.2vh solid lightgray',
+      borderRadius: '5vh 0 5vh 0',
+      overflow: 'hidden',
+      background: whiteColor,
+      boxShadow: 'rgba(0, 0, 0, 0.35) 0px 2vh 10vh',
+    },
+    loginImage: {
+      height: '100%',
+      width: '50%',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundImage: 'url("public/assets/img/login/background.jpg")',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+    },
+    overlay: {
+      height: '100%',
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      background: overlayColor,
+    },
+    heading: {
+      fontSize: '5vh',
+      width: '70%',
+      color: whiteColor,
+    },
+    para: {
+      fontSize: '2vh',
+      width: '70%',
+      color: whiteColor,
+    },
+    customBtn: {
+      color: whiteColor,
+      textDecoration: 'none',
+      background: mainColor,
+      border: 'none',
+      padding: '0 2vw',
+      height: '6vh',
+      borderRadius: '1.5vh',
+      textTransform: 'uppercase',
+      fontSize: '2.2vh',
+    },
+    loginBox: {
+      height: '100%',
+      width: '50%',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  };
+
   return (
-    <div className='main_login_container'>
-      <div className="login_container">
-        <div className="login_image">
-            <div className="overlay">
-                <div className="heading">Don't Have an Account?</div>
-                <div className="para">If you don't have an account, you can click the button below for quick registration.</div>
-                <br />
-                <button className="custom_btn" id="btn_register">Sign Up</button>
+    <div style={styles.root}>
+      <div style={styles.loginContainer}>
+        <div style={styles.loginImage}>
+          <div style={styles.overlay}>
+            <div style={styles.heading}>Don't Have an Account?</div>
+            <div style={styles.para}>
+              If you don't have an account, you can click the button below for quick registration.
             </div>
+            <br />
+            <button style={styles.customBtn} id="btn_register">
+              Sign Up
+            </button>
+          </div>
         </div>
-        <div className="login_box"></div>
+        <div style={styles.loginBox}></div>
       </div>
     </div>
-  )
+  );
 }
