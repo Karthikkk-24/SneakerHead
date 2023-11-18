5f5e911941d30b23e69c162a12fb25ceccef4d2e
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2023-11-18 07:48:17 +0530

    Added Login Page Structure

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import "../assets/css/login.css";
 
 export default function Login() {
   return (
-    <div>
-      
+    <div className='main_login_container'>
+      <div className="login_container">
+        <div className="login_image">
+            <div className="overlay">
+                <div className="heading">Don't Have an Account?</div>
+                <div className="para">If you don't have an account, you can click the button below for quick registration.</div>
+                <br />
+                <button className="custom_btn" id="btn_register">Sign Up</button>
+            </div>
+        </div>
+        <div className="login_box"></div>
+      </div>
     </div>
   )
 }
