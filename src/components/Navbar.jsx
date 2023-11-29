1e1a3ce2d701d4c4f936ddf0b2298572c528c07f
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2023-11-29 08:52:06 +0530

    Added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <div className="logo_div"></div>
+      <div className="navbar_nav"></div>
+      <div className="login">
+        <div className="login_button">Login</div>
+      </div>
+    </div>
+  )
+}
